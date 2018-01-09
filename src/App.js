16935c84d19c848ae48b37272f07db7822eee4a4
DalYoon <yeodal89@gmail.com>
2018-01-09 14:47:32 +0900

    movie_app_done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
 
 class App extends Component {
+
+  state = {};
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+      title={ movie.title }
+      poster={ movie.medium_cover_image }
+      genres={ movie.genres }
+      synopsis={ movie.synopsis }
+      key={ movie.id }
+      />
+    })
+    return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({ movies })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    .then(movieData => movieData.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={ movies ? "App" : "App--loading"} >
+        { movies ? this._renderMovies() : "Loading..." }
       </div>
     );
   }
-}
+};
 
 export default App;
